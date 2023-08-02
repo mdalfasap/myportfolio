2b177a5e1c5e13be87d18bca3a0192df0fca080b
muhammadfarhankt <farhanlatheefkt@gmail.com>
2023-08-02 21:59:15 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,174 @@
+(function ($) {
+	"use strict";
+	var nav = $('nav');
+  var navHeight = nav.outerHeight();
+  
+  $('.navbar-toggler').on('click', function() {
+    if( ! $('#mainNav').hasClass('navbar-reduce')) {
+      $('#mainNav').addClass('navbar-reduce');
+    }
+  })
+ 
+  // Preloader
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+	/*--/ Star ScrollTop /--*/
+	$('.scrolltop-mf').on("click", function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+	/*--/ Star Counter /--*/
+	$('.counter').counterUp({
+		delay: 15,
+		time: 2000
+	});
+
+	/*--/ Star Scrolling nav /--*/
+	$('a.js-scroll[href*="#"]:not([href="#"])').on("click", function () {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top - navHeight + 5)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll').on("click", function () {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#mainNav',
+		offset: navHeight
+	});
+	/*--/ End Scrolling nav /--*/
+
+	/*--/ Navbar Menu Reduce /--*/
+	$(window).trigger('scroll');
+	$(window).on('scroll', function () {
+		var pixels = 50; 
+		var top = 1200;
+		if ($(window).scrollTop() > pixels) {
+			$('.navbar-expand-md').addClass('navbar-reduce');
+			$('.navbar-expand-md').removeClass('navbar-trans');
+		} else {
+			$('.navbar-expand-md').addClass('navbar-trans');
+			$('.navbar-expand-md').removeClass('navbar-reduce');
+		}
+		if ($(window).scrollTop() > top) {
+			$('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
+		} else {
+			$('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
+		}
+	});
+
+	/*--/ Star Typed /--*/
+	if ($('.text-slider').length == 1) {
+    var typed_strings = $('.text-slider-items').text();
+		var typed = new Typed('.text-slider', {
+			strings: typed_strings.split(','),
+			typeSpeed: 80,
+			loop: true,
+			backDelay: 1100,
+			backSpeed: 30
+		});
+	}
+
+	/*--/ Testimonials owl /--*/
+	$('#testimonial-mf').owlCarousel({
+		margin: 20,
+		autoplay: true,
+		autoplayTimeout: 4000,
+		autoplayHoverPause: true,
+		responsive: {
+			0: {
+				items: 1,
+			}
+		}
+	});
+	
+
+})(jQuery);
+function validate(){
+      
+  var name=document.getElementById("name");
+  var email=document.getElementById("email");
+  var subject=document.getElementById("subject");
+  var message=document.getElementById("message");
+  submitOK = "true";
+
+  if(name.value==""){
+    alert("Must Enter a Name");               
+    submitOK = "false";
+  }
+  else if(email.value==""){
+    alert("Enter Email ID");
+    submitOK = "false";       
+    
+  }
+  else if(subject.value==""){
+    alert("Enter Subject"); 
+    submitOK = "false";       
+    
+  }
+  else if(message.value==""){
+    alert("Enter Message"); 
+    submitOK = "false";      
+            
+  }
+  else if(submitOK == "false"){
+    return false;
+  }         
+  
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw0SAFqKLkyAk9k37KNjeK72XYF6lDlcf-Jb-hOg5MFbepjZ1B_NyWRsVf5B7HOanEvLw/exec'
+const form = document.forms['contactForm']
+const msg = document.getElementById("msg");
+
+
+form.addEventListener('submit', e => {           
+e.preventDefault()
+if(submitOK == "true"){    
+fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => {
+    alert("Message Sent Succesfully") 
+    form.reset()
+  })
+
+  .catch(error => console.error('Error!', error.message))
+}
+}  
+  )
+
+
+
+ 
+
